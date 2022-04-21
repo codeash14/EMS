aae0227a61a4c67ee28c62c6edc8ea0cb75b3338
codeash14 <ayushiash1412@gmail.com>
2022-04-21 15:06:26 +0530

    commit D

--- app/src/main/java/com/jas/ems/SplashActivity.kt
+++ app/src/main/java/com/jas/ems/SplashActivity.kt
@@ -14,10 +14,10 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         // This is used to hide the status bar and make
         // the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
 
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
