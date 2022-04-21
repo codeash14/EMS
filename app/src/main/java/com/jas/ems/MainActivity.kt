2e43b6d63bfa03e74e9101eb83f9e26d9e6b7ec7
codeash14 <ayushiash1412@gmail.com>
2022-04-21 18:11:21 +0530

    commit D

--- app/src/main/java/com/jas/ems/MainActivity.kt
+++ app/src/main/java/com/jas/ems/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.jas.ems
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.jas.ems.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var user: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        user=FirebaseAuth.getInstance()
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.signOut->{
+                user.signOut()
+                startActivity(
+                    Intent(this, SplashActivity::class.java)
+                )
+                Toast.makeText(this, "Sign Out Successful!", Toast.LENGTH_SHORT).show()
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
     }
 }
